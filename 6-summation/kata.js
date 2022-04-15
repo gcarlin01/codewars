3ebee79cfb288b545381d15050ca3a9602fa5180
gcarlin01 <giancarlo.carlin01@gmail.com>
2022-04-14 22:12:16 -0400

    added test for unsupported when num is 0

--- 6-summation/kata.js
+++ 6-summation/kata.js
@@ -1,10 +1,14 @@
 var summation = function (num) {
-   let counter = 0
-
-    for (let i = 0; i <= num; i ++){
-        counter += i
-    }
-return counter 
+  if (num[0] === 0) {
+    return "unsupported";
   }
 
-  export default summation
+  let counter = 0;
+
+  for (let i = 0; i <= num; i++) {
+    counter += i;
+  }
+  return counter;
+};
+
+export default summation;
--- 6-summation/kata.test.js
+++ 6-summation/kata.test.js
@@ -4,6 +4,9 @@ describe ('summation', function (){
     it('returns the summation of every number from 1 to num', function (){
         expect(summation([4])).toBe(10)
         expect(summation([1])).toBe(1)
-       
+    })
+
+    it('returns "unsupported" when number is 0', () => {
+        expect(summation([0])).toBe("unsupported")
     })
 })
